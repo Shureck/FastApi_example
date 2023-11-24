3e9249829e84cd7369164b3695afc3015eeb3e81
Alexander Lev <Sashalev200149@gmail.com>
2023-11-24 13:15:42 +0300

    first commit

---
+++ project/main.py
@@ -0,0 +1,15 @@
+import uvicorn
+from fastapi import FastAPI, File, UploadFile, status, Header, Request, Depends
+from fastapi.staticfiles import StaticFiles
+app = FastAPI()
+
+count = 0
+
+@app.get("/")
+async def getR():
+    return "Test"
+
+@app.get("/get")
+async def getUser(count):
+    count += count
+    return {"count":count}
